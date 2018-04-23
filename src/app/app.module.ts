3d2369a468be6fa70769f36a5f3d161af955d4cd
a3loko <lokesh09rishi@gmail.com>
2018-04-23 18:30:34 +0530

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { GetOtpComponent } from './get-otp/get-otp.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetOtpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
